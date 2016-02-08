fb65d05c91612f3026f2d128888bcc90fa52ad6d
Ray Butcher <ray@bashton.com>
2016-02-08 14:13:49 +0000

    Fix path parameter validation
    The example also had to be updated to run validation within the
    router, to give it access to the path parameters that it sets.

--- examples/test.js
+++ examples/test.js
@@ -1,7 +1,28 @@
 var app = require('./basic.js');
+var assert = require('assert');
 var request = require('supertest').agent(app.listen());
 
 describe('Validating', function () {
+    it('GET /users/123 should return 200', function (done) {
+        request
+            .get('/users/123')
+            .expect("X-fleek-validation", "OK")
+            .expect(200)
+            .end(done);
+    });
+    it('GET /users/abc should return 400', function (done) {
+        request
+            .get('/users/abc')
+            .expect("X-fleek-validation", "KO")
+            .expect(400)
+            .end(function(err, res) {
+                assert.equal(res.body.error_name, 'VALIDATION_FAILED');
+                assert.equal(res.body.details[0].name, 'TYPE.INTEGER');
+                assert.equal(res.body.details[0].code, 102);
+                assert.equal(res.body.details[0].message, 'Integer expected');
+                done();
+            });
+    });
     it('POST /users should return 201', function (done) {
         request
             .post('/users')
@@ -17,5 +38,5 @@ describe('Validating', function () {
             .expect("X-fleek-validation", "KO")
             .expect(400)
             .end(done);
-    })
+    });
 })
--- examples/users.js
+++ examples/users.js
@@ -1,15 +1,24 @@
 /**
+ * GET /users/{id}
+ */
+module.exports.get = function *(next) {
+    this.status = 200;
+    this.body = {
+        id: this.params.id
+    }
+};
+
+/**
  * POST /users {name}
  */
 module.exports.post = function *(next) {
     try {
-        console.log(JSON.stringify(this.request.body) + ' created');
         this.status = 201;
+        this.body = {
+            created: this.request.body
+        };
     } catch (e) {
         this.status = 500;
-        this.response.body = e;
+        this.body = e;
     }
 };
-
-
-
