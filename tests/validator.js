df9f3e55b224ef80574e5ef0bfe205e3d7a73299
johnhof <john.hofrichter@gmail.com>
2016-12-02 15:07:37 -0800

    implemented .one tests

--- tests/validator.js
+++ tests/validator.js
@@ -4,6 +4,7 @@ const mocha = require('co-mocha');
 const expect = require('chai').expect;
 
 const Validator = require('../lib/validator');
+const ValErr = require('../lib/error');
 const SWAGGER = require('./swagger.json');
 const CTX = require('./ctx');
 
@@ -18,57 +19,59 @@ describe('Validator', () => {
     //   expect(() => new Validator({})).to.throw(Error);
     // });
   });
-  describe('ctx', () => {
-    it('should ignore requests with no fleek context', () => {
-      let validator = new Validator();
-      console.log(validator.ctx({}));
-    });
-    it('should return success for requests passing validation', () => {
-      let validator = new Validator();
-      let ctx = CTX.post('/user/create')
-                    .context(SWAGGER.paths['/user/create'].post)
-                    .body({
-                      email: 'test@testing.com',
-                      primary_phone: '1112223333',
-                      birthdate: '01/01/1991',
-                      name: {
-                        first: 'john',
-                        last: 'hof'
-                      }
-                    });
-      console.log(validator.ctx(ctx));
-    });
-    it('should return error for requests failing validation', () => {
-      let validator = new Validator();
-      console.log(validator.ctx(new CTX()));
-    });
-  });
-  describe('object', () => {
-    it('should ignore requests with no fleek context', () => {
-      let validator = new Validator();
-      console.log(validator.ctx(new CTX()));
-    });
-    it('should return success for requests passing validation', () => {
-      let validator = new Validator();
-      console.log(validator.ctx(new CTX()));
-    });
-    it('should return error for requests failing validation', () => {
-      let validator = new Validator();
-      console.log(validator.ctx(new CTX()));
-    });
-  });
+  // describe('ctx', () => {
+  //   it('should ignore requests with no fleek context', () => {
+  //     let validator = new Validator();
+  //     console.log(validator.ctx({}));
+  //   });
+  //   it('should return succeed for requests passing validation', () => {
+  //     let validator = new Validator();
+  //     let ctx = CTX.post('/user/create')
+  //                   .context(SWAGGER.paths['/user/create'].post)
+  //                   .body({
+  //                     email: 'test@testing.com',
+  //                     primary_phone: '1112223333',
+  //                     birthdate: '01/01/1991',
+  //                     name: {
+  //                       first: 'john',
+  //                       last: 'hof'
+  //                     }
+  //                   });
+  //   });
+  //   it('should return error for requests failing validation', () => {
+  //     let validator = new Validator();
+  //   });
+  // });
+  // describe('object', () => {
+  //   it('should ignore requests with no fleek context', () => {
+  //     let validator = new Validator();
+  //   });
+  //   it('should return success for requests passing validation', () => {
+  //     let validator = new Validator();
+  //   });
+  //   it('should return error for requests failing validation', () => {
+  //     let validator = new Validator();
+  //   });
+  // });
   describe('one', () => {
-    it('should ignore requests with no fleek context', () => {
-      let validator = new Validator();
-      console.log(validator.ctx(new CTX()));
+    let param = () => { return { required: true, email: true, type: 'string' }; };
+    let validator = new Validator();
+    it('should return the value if using validations pass', () => {
+      let val = 'test@blackhole.com';
+      let result = validator.one(val, param());
+      expect(result).to.equal(val);
     });
-    it('should return success for requests passing validation', () => {
-      let validator = new Validator();
-      console.log(validator.ctx(new CTX()));
+    it('should return a ValidationError if validation fails', () => {
+      let val = 'testblackhole.com';
+      let result = validator.one(val, param());
+      expect(result).instanceof(ValErr);
     });
-    it('should return error for requests failing validation', () => {
-      let validator = new Validator();
-      console.log(validator.ctx(new CTX()));
+    it('should return an updates value if a normalization is used', () => {
+      let val = 'TeSt@BlAcKhOlE.cOm';
+      let parameter = param();
+      parameter.toLowerCase = true;
+      let result = validator.one(val, parameter);
+      expect(result).to.equal(val.toLowerCase());
     });
   });
 });
